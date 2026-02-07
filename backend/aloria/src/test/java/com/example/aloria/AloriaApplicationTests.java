bf4dfaae2fc42a900d464fc21c2fcfa03c1133d9
LAB13_USER ON WS09 <L13Y09W09@LabHCI.net>
2026-02-07 09:57:50 +0800

    setup frontEnd (React+vite) & backend (SpringBoot)

---
+++ backend/aloria/src/test/java/com/example/aloria/AloriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aloria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
